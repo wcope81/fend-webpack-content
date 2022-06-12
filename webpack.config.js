15e210372096c9f8388eb278a99324173857b835
wcope81 <wcopel1@wgu.edu>
2022-06-12 16:27:10 -0500

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,12 @@ const webpack = require('webpack')
 module.exports = {
     entry: './src/client/index.js',
     devtool: 'source-map',
+    mode: "development",
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
     }
 }
