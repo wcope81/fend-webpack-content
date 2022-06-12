cce8292dde4f741417f9d3d00b4a5eacd8077839
wcope81 <wcopel1@wgu.edu>
2022-06-12 17:23:08 -0500

    commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,16 +3,15 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
+    mode: 'production',
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
     },
     plugins: [
         new HtmlWebPackPlugin({
